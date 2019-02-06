91ae15b45e4f62678d2176721ebdaef540584fcb
Akanksha <akanksharajput60@gmail.com>
2019-02-06 00:16:05 -0500

    first

---
+++ task1/test_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 15:31:24 2019
+
+@author: akanksha_rajput
+"""
+from task12 import fib
+def test_fib():
+    assert fib(2) == 1
+    assert fib(5) == 5
+    assert fib(12) == 144
+    
+    
+ 
